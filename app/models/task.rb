01053df8dd95c66dd6d7992fd49aa6c44a79925c
Marat Gumerov <gumerovm74dev@gmail.com>
2022-10-05 20:14:33 +0300

    added state to Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,38 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
+
+  state_machine initial: :new_task do
+    # state :new_task
+    # state :in_development
+    # state :in_qa
+    # state :in_code_review
+    # state :ready_for_release
+    # state :released
+    # state :archived
+
+    event :archive do
+      transition [:new_task, :released] => :archived
+    end
+
+    event :in_develop do
+      transition [:new_task, :in_qa, :in_code_review] => :in_development
+    end
+
+    event :review do
+      transition in_qa: :in_code_review
+    end
+
+    event :qa do
+      transition in_develop: :in_qa
+    end
+
+    event :check_release do
+      transition in_code_review: :ready_for_release
+    end
+
+    event :release do
+      transition ready_for_release: :released
+    end
+  end
 end
