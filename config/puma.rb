e91d7a6bcd5894533211f85a5dc3891bf469ca94
Marat gumerov <gumerovm74dev@gmail.com>
2022-10-12 22:40:33 +0300

    added links & ref port

--- config/puma.rb
+++ config/puma.rb
@@ -14,8 +14,9 @@ threads min_threads_count, max_threads_count
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+set_default_host '0.0.0.0'
 #
-port ENV.fetch("PORT") { 3000 }
+port ENV.fetch("PORT") { 8080 }
 
 # Specifies the `environment` that Puma will run in.
 #
